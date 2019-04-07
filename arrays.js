bbba43bd643c209b51b09a125daf54d26125ecd9
Hannah Matthewson <hannahmatthewson2@gmail.com>
2019-04-07 14:22:44 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars ["snickers", "hundred grand", "kitkat", "skittles"];
+
+addElementToBeginningOfArray(cats, moreCats) {
+  const cats = ["Milo", "Garfield"];
+  const moreCats = ["Felix", ...cats];
+  return addElementToBeginningOfArray();
+}
+
+destructivelyAddElementToBeginningOfArray() {
+  
+}
