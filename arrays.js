8613c1cc298bdb0a96e2a47e28f7dbde31b02982
Hannah Matthewson <hannahmatthewson2@gmail.com>
2019-04-07 14:24:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-var chocolateBars ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 addElementToBeginningOfArray(cities) {
   var cities = ["New York", "San Francisco"];
